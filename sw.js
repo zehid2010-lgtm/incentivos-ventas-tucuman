9fdb84f5579e332cb4bde2510f4b32a69ed2a9e4
zehid2010-lgtm <zehid2010@gmail.com>
2026-09-21 12:35:59 -0300

    Restaurar Incentivos y limpiar caché antigua

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "incentivos-shell-v1";
+const CACHE = "incentivos-shell-v3";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/icon-192.png", "./assets/icon-512.svg"];
 
 self.addEventListener("install", event => {
